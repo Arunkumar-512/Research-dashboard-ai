d0382ac3652b9b6c8e67b511f44693aab34a6198
Arunkumar <arunkumar283512@gmail.com>
2026-07-25 23:48:02 +0530

    small changes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -157,8 +157,12 @@ export default function ResearchDashboard() {
 
     return () => clearInterval(interval);
   }, []);
+
+
+
   const API_URL =
     process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+    console.log(API_URL);
 
   const checkBackendStatus = async () => {
     try {
